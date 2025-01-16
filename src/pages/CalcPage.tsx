048b073268d38af176db9ef882fddd0be88402c0
Andrzej Korbiel <andkorbiel@gmail.com>
2025-01-16 11:03:51 +0100

    add basic structure

---
+++ src/pages/CalcPage.tsx
@@ -0,0 +1,9 @@
+import { Container, Typography } from "@mui/material";
+
+export const CalcPage = () => {
+  return (
+    <Container maxWidth="xl">
+      <Typography variant="h1">About what?</Typography>
+    </Container>
+  );
+};
---
+++ src/pages/index.ts
@@ -0,0 +1,2 @@
+export * from "./CalcPage";
+export * from "./Home";
